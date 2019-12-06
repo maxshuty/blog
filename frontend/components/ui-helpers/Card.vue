243fb18dbd284636dd3533fabfe5ef8c3df3fdf5
PMDOMHQ\max.poshusta <max.poshusta@protolabs.com>
2019-12-06 08:20:52 -0600

    Adding reusable card component

---
+++ frontend/components/ui-helpers/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-hover v-slot:default="{hover}">
+    <v-card
+      :elevation="hover ? 6 : 2"
+      :disabled="disabled"
+      :height="height"
+      :width="width"
+      class="mx-auto"
+    >
+      <v-card-text class="my-4 text-center title">
+        <div>
+          <h3>{{ title }}</h3>
+          <div v-html="$md.render(body)" />
+        </div>
+      </v-card-text>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  name: 'CardComp',
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    body: {
+      type: String,
+      default: '',
+      required: false
+    },
+    height: {
+      type: Number,
+      default: 350
+    },
+    width: {
+      type: Number,
+      default: 350
+    }
+  }
+};
+</script>
