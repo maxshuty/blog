23474e442bf97f90c0860bc3cf9b348094708be8
PMDOMHQ\max.poshusta <max.poshusta@protolabs.com>
2019-12-06 10:24:58 -0600

    Adding search component

---
+++ frontend/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent>
+    <span />
+    <v-col cols="12" sm="6" md="3">
+      <v-text-field
+        v-model="query"
+        :clearable="clearable"
+        label="Search"
+        type="search"
+        @input="() => $emit('input', query)" />
+    </v-col>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'SearchComp',
+  props: {
+    clearable: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data() {
+    return {
+      query: ''
+    };
+  }
+};
+</script>
