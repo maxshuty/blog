1634cac4461a1b8c312b7e12bd12e58195b3a621
PMDOMHQ\max.poshusta <max.poshusta@protolabs.com>
2019-12-13 11:10:55 -0600

    Querying the new blog by ID query we added and stubbing out the blog.title on the /blog page

---
+++ frontend/pages/blog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{ blog.title }}</h1>
+  </div>
+</template>
+
+<script>
+import blogQuery from '../apollo/queries/blog/blog.gql';
+
+export default {
+    name: 'Blog',
+    data() {
+        return {
+            blog: Object
+        };
+    },
+    apollo: {
+        blog: {
+            prefetch: true,
+            query: blogQuery,
+            variables() {
+                return { id: this.$route.params.id };
+            }
+        }
+    }
+};
+</script>
