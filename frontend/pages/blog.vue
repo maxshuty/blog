1019c75d4882d75e6c81f67903c66fafd89a2662
PMDOMHQ\max.poshusta <max.poshusta@protolabs.com>
2019-12-13 11:19:07 -0600

    Adding todo's to move queries to the store

--- frontend/pages/blog.vue
+++ frontend/pages/blog.vue
@@ -14,6 +14,8 @@ export default {
             blog: Object
         };
     },
+    // TODO: Max P - it would be awesome to move these queries to actions in the store...
+    // see (about half way down): https://lmiller1990.github.io/electic/posts/integrating_apollo_with_vue_and_vuex.html?source=post_page---------------------------
     apollo: {
         blog: {
             prefetch: true,
