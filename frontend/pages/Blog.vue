5d233a53c072adc2e8fad4a8fff1381040ff287a
PMDOMHQ\max.poshusta <max.poshusta@protolabs.com>
2019-12-13 07:54:16 -0600

    Adding new Blog page shell to display a vlog

---
+++ frontend/pages/Blog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hello world - {{ id }}</div>
+</template>
+
+<script>
+export default {
+    name: 'Blog',
+    props: {
+        id: {
+            type: Number,
+            required: true
+        }
+    }
+};
+</script>
