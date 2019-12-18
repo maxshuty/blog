a97f30efb41ea52c70d9b0156031d20dd79ae0aa
PMDOMHQ\max.poshusta <max.poshusta@protolabs.com>
2019-12-18 11:02:32 -0600

    Fixing bug where title was taking up space even if not provided

--- frontend/components/ui-helpers/Card.vue
+++ frontend/components/ui-helpers/Card.vue
@@ -6,7 +6,7 @@
       :height="height"
       :width="width"
       class="mx-auto">
-      <v-card-text class="my-4 text-center title">
+      <v-card-text v-if="title" class="my-4 text-center title">
         <h3>{{ title }}</h3>
       </v-card-text>
       <v-card-text>
